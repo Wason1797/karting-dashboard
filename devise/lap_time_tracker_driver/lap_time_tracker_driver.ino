c7d14de3aa44fc650c2c340b30633bf362b9cec8
Wladymir Brborich <wabrborich@hotmail.com>
2022-11-19 17:31:03 -0500

    Add coordinate manager and stop watch classes
    * Add wrapper code to handle common operations like keeping time, and math.

--- devise/lap_time_tracker_driver/lap_time_tracker_driver.ino
+++ devise/lap_time_tracker_driver/lap_time_tracker_driver.ino
@@ -13,76 +13,134 @@ byte month, day, hour, minute, second, hundredths;
 
 unsigned long chars;
 unsigned short sentences, failed_checksum;
+
 float current_latitude, current_longitude;
-float closest_lat_to_start = 0;
-float closest_long_to_start = 0;
-float start_latitude, start_longitude;
 bool running = false;
-float start_lap_time = 0;
-float current_lap_time = 0;
-float MIN_LAP_TIME = 25;  // TODO Define this correctly
-float best_distance = 0;
-float stop_time = 0;
 
-bool start_pressed() {
-  // TODO: Implement start pressed
-  return true;
-}
 
-float square_distance_between_coordinates(float start_lat, float start_long, float end_lat, float end_long) {
-  return ((end_lat - start_lat) * (end_lat - start_lat)) - (end_long - start_long) * (end_long - start_long);
-}
+class Stopwatch {
 
-bool is_in_elipse(float latitude, float longitude) {
-  return false;  //TODO implement
-}
+private:
+  bool running = false;
+  unsigned long start_time = 0;
+  unsigned long stop_time = 0;
+  unsigned long offset = 0;
 
-void start_stopwatch() {
-  start_lap_time = millis();
-}
 
-void stop_stopwatch() {
- stop_time = millis();
-}
+public:
+  Stopwatch() {}
 
-void reset_stopwatch() {
- start_lap_time = 0;
- stop_time = millis();
-}
+  void start() {
+    if (running) return;
+    start_time = millis();
+    running = true;
+    stop_time = 0;
+  }
+
+
+  void stop() {
+    if (!running) return;
+    stop_time = millis();
+    running = false;
+  }
+
+  void reset() {
+    running = true;
+    stop_time = 0;
+    offset = 0;
+    start_time = millis();
+  }
+
+  unsigned long elapsed() {
+    if (running) return 0;
+    return (stop_time - start_time) + offset;
+  }
+
+  unsigned long now() {
+    if (!running) return 0;
+    return (millis() - start_time) + offset;
+  }
+
+  void set_offset(unsigned long new_offset) {
+    offset = new_offset;
+  }
+
+};
+
+
+struct Point {
+  double x;
+  double y;
+};
+
+
+class CoordinateMath {
+
+private:
+  double rotation_angle = 0;
+  Point starting_point{ 0, 0 };
+  Point reference_point = { 0, 0 };
+
+  double sin_factor = 0;
+  double cos_factor = 0;
+
+
+public:
+
+  CoordinateMath() {}
+
+  Point translate_origin(Point position) {
+    return Point{
+      position.x - starting_point.x, position.y - starting_point.y
+    };
+  }
+
+  void set_starting_point(float latitude, float longitude) {
+    starting_point.x = static_cast<double>(longitude);
+    starting_point.y = static_cast<double>(latitude);
+  }
+
+  void set_reference_point(float latitude, float longitude) {
+    reference_point.x = static_cast<double>(longitude);
+    reference_point.y = static_cast<double>(latitude);
+  }
+
+  void calculate_rotation_factors() {
+    Point translated_reference = translate_origin(reference_point);
+    rotation_angle = atan2(translated_reference.y, translated_reference.x);
+    sin_factor = sin(rotation_angle);
+    cos_factor = cos(rotation_angle);
+  }
+
+  Point rotate(Point position) {
+    return Point{
+      (cos_factor * position.x - sin_factor * position.y), (cos_factor * position.y + sin_factor * position.x)
+    };
+  }
+
+};
+
 
 
 void print_gps_data() {
-  // TODO Output to display
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
-  Serial.print("Fecha: ");
-  Serial.print(day, DEC);
-  Serial.print("/");
-  Serial.print(month, DEC);
-  Serial.print("/");
-  Serial.print(year);
-  Serial.print(" Hora: ");
-  Serial.print(hour, DEC);
-  Serial.print(":");
-  Serial.print(minute, DEC);
-  Serial.print(":");
-  Serial.print(second, DEC);
-  Serial.print(".");
-  Serial.println(hundredths, DEC);
-  Serial.print("Altitud (metros): ");
-  Serial.println(gps.f_altitude());
-  Serial.print("Rumbo (grados): ");
-  Serial.println(gps.f_course());
-
-  Serial.print("Satelites: ");
-  Serial.println(gps.satellites());
-  Serial.println();
+ 
 }
 
 void print_laptime_info() {
   // TODO Implement
 }
 
+bool start_pressed() {
+  return false;
+}
+
+Stopwatch stopwatch;
+CoordinateMath coordinate_math;
+
 void setup() {
+  stopwatch = Stopwatch();
+  coordinate_math = CoordinateMath();
+
   Serial.begin(115200);   //Iniciamos el puerto serie
   serialgps.begin(9600);  //Iniciamos el puerto serie del gps
   //Imprimimos:
@@ -92,6 +150,8 @@ void setup() {
   Serial.println();
 }
 
+
+
 void loop() {
   while (serialgps.available()) {
     int gps_data = serialgps.read();
@@ -99,41 +159,21 @@ void loop() {
     if (gps.encode(gps_data)) {
       gps.f_get_position(&current_latitude, &current_longitude);
 
-      while (!running) {  // TODO: Check if we need this loop or the ESP is fast enough to just have a conditional
+      while (!running) {
         gps.f_get_position(&current_latitude, &current_longitude);
-        if (start_pressed()) {
-          start_latitude = current_latitude;
-          start_longitude = current_longitude;
-          running = true;
-          start_stopwatch();
-          break;
-        }
-        print_gps_data();
-      }
 
+        if(start_pressed()){
+          coordinate_math.set_starting_point(current_latitude, current_longitude);
+          delay(1000);
+          gps.f_get_position(&current_latitude, &current_longitude);
+          coordinate_math.set_reference_point(current_latitude, current_longitude);
+          stopwatch.start();
+        }
+
+
+      }
       if (running) {
         float speed = gps.f_speed_kmph();
-        current_lap_time = millis();
-        Serial.print("Velocidad(kmph): ");
-        Serial.println(speed);
-
-        while (is_in_elipse(current_latitude, current_longitude) && current_lap_time >= MIN_LAP_TIME) {
-          float current_best_distance = square_distance_between_coordinates(current_latitude, start_latitude, current_longitude, start_longitude);
-          if (best_distance == 0 || current_best_distance <= best_distance) {  // ? Could use FLT_MAX to initialize best distance
-            best_distance = current_best_distance;
-            stop_time = current_lap_time;
-          }
-          gps.f_get_position(&current_latitude, &current_longitude);
-        }
-
-        if (stop_time > 0) {
-          reset_stopwatch();
-          stop_time = 0;
-          best_distance = 0;
-        }
-
-        print_gps_data();
-        print_laptime_info();
       }
     }
   }
